685f00f0ae432a1f3e4b28982b6244e39c6c97bc
Adam <bosak3@gmail.com>
2022-02-09 12:59:23 -0600

    minor changes to Css

--- client/src/cards/GameCard.js
+++ client/src/cards/GameCard.js
@@ -8,9 +8,10 @@ function GameCard ({gameData}){
         <div>
             <h1>{gameData.title}</h1>
             <img src={gameData.image_url} alt={gameData.title} className="items"/>
-            <br></br>
-            <a href={gameData.purchase_url}>Purchase</a>
             <h3>{gameData.genre}</h3>
+            <a href={gameData.purchase_url}>Purchase</a>
+            <br></br>
+            <br></br>
             <ReviewForm gameData={gameData}/>
         </div>
 
